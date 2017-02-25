aed315f532d71fcd410a577ff8e356140b7a0f71
purva5194 <purvaben.patel@sjsu.edu>
2017-02-24 18:05:05 -0800

    Server file updated

--- nado.py
+++ nado.py
@@ -1,43 +1,43 @@
 import os
-
+import base64
 import requests
 import tornado.ioloop
 import tornado.web
 
+class getEncodedUrlHandler(tornado.web.RequestHandler):
 
-GEOCODE_FMT = 'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={key}'
+	def set_default_headers(self):
+		# allow cross-origin requests from the pdf creating app on DroneDeploy to the heroku server
+		self.set_header("Access-Control-Allow-Origin", "https://www.dronedeploy.com")
+		self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+		# allow POST method
+		self.set_header("Access-Control-Allow-Methods", "POST, OPTIONS")
 
+	def post(self):
+		# decode the request body to JSON
+		data = tornado.escape.json_decode(self.request.body)
 
-class GeocodeHandler(tornado.web.RequestHandler):
-    """Proxy a call to the Google Maps geocode API"""
+		# list of tile image URLs will be pushed into the following list and sent to client
+		encoded_tiles = []
 
-    def set_default_headers(self):
-        # allow cross-origin requests to be made from your app on DroneDeploy to your web server
-        self.set_header("Access-Control-Allow-Origin", "https://www.dronedeploy.com")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        # add more allowed methods when adding more handlers (POST, PUT, etc.)
-        self.set_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+		for tile in data['tile']:
+			# get results from the given URL including tile image
+			res = requests.get(tile)
+			# encode the tile png into base64
+			encoded = base64.b64encode(res.content)
+			encoded_tiles.append(encoded)
 
-    def get(self):
-        api_key = os.environ.get("API_KEY")
-        address = self.get_query_argument("address")
-        url = GEOCODE_FMT.format(address=address, key=api_key)
+		# send the results as JSON format back to the client
+		self.write({'msg': encoded_tiles})
 
-        # fetch results of the geocode from Google
-        response = requests.get(url)
-
-        # send the results back to the client
-        self.write(response.content)
-
-    def options(self):
-        # no body
-        self.set_status(204)
-        self.finish()
-
+	def options(self):
+		# no body
+		self.set_status(204)
+		self.finish()
 
 def main():
     application = tornado.web.Application([
-        (r"/geocode/", GeocodeHandler)
+        (r"/getEncodedUrl/", getEncodedUrlHandler)
     ])
     port = int(os.environ.get("PORT", 5000))
     application.listen(port)
